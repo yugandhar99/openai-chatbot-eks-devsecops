5cce68a7a9278a4e50a5435327cd4df6c6682250
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-10 18:22:07 -0500

    Update iam-policy.tf

--- Chatbot-UI/EKS-TF/iam-policy.tf
+++ Chatbot-UI/EKS-TF/iam-policy.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.EKSClusterRole.name
-
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
