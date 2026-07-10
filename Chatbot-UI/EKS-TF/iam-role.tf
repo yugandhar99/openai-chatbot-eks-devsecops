ea2ec55a1f4d502793ba067eb188d5f845ac63cd
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-10 18:22:18 -0500

    Update iam-role.tf

--- Chatbot-UI/EKS-TF/iam-role.tf
+++ Chatbot-UI/EKS-TF/iam-role.tf
@@ -4,6 +4,7 @@ resource "aws_iam_role" "EKSClusterRole" {
     Version = "2012-10-17"
     Statement = [
       {
+
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
