c7ad46b9db3f48b18418e74f5dc67c364a6d59f2
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-19 14:38:17 -0500

    Update importExports.test.ts

--- Chatbot-UI/__tests__/utils/app/importExports.test.ts
+++ Chatbot-UI/__tests__/utils/app/importExports.test.ts
@@ -3,7 +3,6 @@ import { OpenAIModels, OpenAIModelID } from '@/types/openai';
 import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
 import { it, describe, expect } from 'vitest';
 
-
 import {
   cleanData,
   isExportFormatV1,
