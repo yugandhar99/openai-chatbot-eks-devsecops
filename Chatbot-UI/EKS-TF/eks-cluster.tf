be24586ea2327ff16c5fb7822611aff961af70c7
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-07 14:18:28 -0500

    Update eks-cluster.tf

--- Chatbot-UI/EKS-TF/eks-cluster.tf
+++ Chatbot-UI/EKS-TF/eks-cluster.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "eks-cluster" {
     subnet_ids         = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
     security_group_ids = [data.aws_security_group.sg-default.id]
   }
- 
+
   version = 1.28
 
   depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
