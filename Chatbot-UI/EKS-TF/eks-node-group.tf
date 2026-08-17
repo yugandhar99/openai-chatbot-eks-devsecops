42eaac97c4ce28a557666f0b3f81884d902566d9
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-17 12:53:20 -0500

    Update eks-node-group.tf

--- Chatbot-UI/EKS-TF/eks-node-group.tf
+++ Chatbot-UI/EKS-TF/eks-node-group.tf
@@ -4,7 +4,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   node_role_arn   = aws_iam_role.NodeGroupRole.arn
   subnet_ids      = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
 
- 
+
   scaling_config {
     desired_size = 2
     max_size     = 3
